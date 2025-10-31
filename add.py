71ab75201e4e6bf199121e91bff6b9c3e02c07e0
Dilmurod Tuxtanazarov <dtuxtanazarov@gmail.com>
2025-10-31 16:26:15 +0500

    git fetch uchun

--- add.py
+++ add.py
@@ -1,3 +1,4 @@
+#git fetch uchun yangi o`zgarishlar
 from telegram import Update
 from telegram.ext import Application, MessageHandler, CommandHandler, filters, ContextTypes
 
---
+++ add3.py
@@ -0,0 +1,1 @@
+#git fetch uchun yangi o`zgarishlar
